a07af74988948f9c49f77d2a82aef443a778c3b1
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2016-10-30 17:13:29 -0400

    Reformatted SQL

--- a5/c_views.sql
+++ a5/c_views.sql
@@ -1,56 +1,56 @@
 CREATE VIEW PLAYER_SEASON_STATS (
-PLAYER_ID,
-SEASON_ID,
-PLAYER_FNAME,
-PLAYER_LNAME,
-AB_AVG, R_AVG,
-RBI_AVG,
-"1B_AVG",
-"2B_AVG",
-"3B_AVG",
-"HR_AVG",
-H,
-BA)
+      PLAYER_ID,
+      SEASON_ID,
+      PLAYER_FNAME,
+      PLAYER_LNAME,
+      AB_AVG, R_AVG,
+      RBI_AVG,
+      "1B_AVG",
+      "2B_AVG",
+      "3B_AVG",
+      "HR_AVG",
+      H,
+      BA)
 AS
-SELECT
-   s.PLAYER_ID,
-   SEASONS.ID,
-   PLAYERS.FIRST_NAME,
-   PLAYERS.LAST_NAME,
-   avg(s.AB),
-   avg(s.R),
-   avg(s.RBI),
-   avg(s."1B"),
-   avg(s."2B"),
-   avg(s."3B"),
-   avg(s.HR),
-   sum(s."1B") + sum(s."2B") + sum(s."3B") + sum(s.HR),
-   (sum(s."1B") + sum(s."2B") + sum(s."3B") + sum(s.HR) ) / sum(s.AB)
+   SELECT
+      s.PLAYER_ID,
+      SEASONS.ID,
+      PLAYERS.FIRST_NAME,
+      PLAYERS.LAST_NAME,
+      avg(s.AB),
+      avg(s.R),
+      avg(s.RBI),
+      avg(s."1B"),
+      avg(s."2B"),
+      avg(s."3B"),
+      avg(s.HR),
+      sum(s."1B") + sum(s."2B") + sum(s."3B") + sum(s.HR),
+      (sum(s."1B") + sum(s."2B") + sum(s."3B") + sum(s.HR) ) / sum(s.AB)
    FROM STATISTICS s
-   INNER JOIN GAMES
-   ON s.GAME_ID = GAMES.ID
-   INNER JOIN SEASONS
-   ON SEASONS.ID = GAMES.SEASON_ID
-   RIGHT OUTER JOIN PLAYERS
-   ON s.PLAYER_ID = PLAYERS.ID
+      INNER JOIN GAMES
+         ON s.GAME_ID = GAMES.ID
+      INNER JOIN SEASONS
+         ON SEASONS.ID = GAMES.SEASON_ID
+      RIGHT OUTER JOIN PLAYERS
+         ON s.PLAYER_ID = PLAYERS.ID
    GROUP BY s.PLAYER_ID, SEASONS.ID, PLAYERS.FIRST_NAME, PLAYERS.LAST_NAME
    ORDER BY s.PLAYER_ID, SEASONS.ID
    WITH READ ONLY;
 
-   CREATE VIEW PLAYER_CAREER_STATS (
-   PLAYER_ID,
-   PLAYER_FNAME,
-   PLAYER_LNAME,
-   AB_CAREER,
-   R_CAREER,
-   RBI_CAREER,
-   "1B_CAREER",
-   "2B_CAREER",
-   "3B_CAREER",
-   "HR_CAREER",
-   "H_CAREER",
-   "BA_CAREER"
-   ) AS
+CREATE VIEW PLAYER_CAREER_STATS (
+      PLAYER_ID,
+      PLAYER_FNAME,
+      PLAYER_LNAME,
+      AB_CAREER,
+      R_CAREER,
+      RBI_CAREER,
+      "1B_CAREER",
+      "2B_CAREER",
+      "3B_CAREER",
+      "HR_CAREER",
+      "H_CAREER",
+      "BA_CAREER"
+) AS
    SELECT
       PLAYER_ID,
       PLAYER_FNAME,
@@ -64,11 +64,6 @@ SELECT
       avg(HR_AVG),
       avg(H),
       avg(BA)
-      FROM PLAYER_SEASON_STATS
-      GROUP BY PLAYER_ID, PLAYER_FNAME, PLAYER_LNAME
-      ORDER BY PLAYER_LNAME, PLAYER_FNAME, PLAYER_ID;
-
-
-
-SELECT * FROM PLAYER_SEASON_STATS;
-SELECT * FROM PLAYER_CAREER_STATS;
+   FROM PLAYER_SEASON_STATS
+   GROUP BY PLAYER_ID, PLAYER_FNAME, PLAYER_LNAME
+   ORDER BY PLAYER_LNAME, PLAYER_FNAME, PLAYER_ID;
